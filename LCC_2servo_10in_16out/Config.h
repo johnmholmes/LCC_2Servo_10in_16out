1a70007ae1ff709e1d2b2feef6ff12ce60816cfb
johnmholmes <60571002+johnmholmes@users.noreply.github.com>
2026-07-13 10:15:19 +0100

    Update Config.h

--- LCC_2servo_10in_16out/Config.h
+++ LCC_2servo_10in_16out/Config.h
@@ -38,7 +38,7 @@
 #define MANU " OpenLCB "                    // The manufacturer of node
 #define MODEL BOARD " 2Servo10in16out "     // The default model of the board
 #define HWVERSION " ESP 1 Beyond "          // Hardware version
-#define SWVERSION " 1.0.0 "                 // Software version
+#define SWVERSION " 1.0.3 "                 // Software version
 
 // Global defs
 const bool USE_90_ON_STARTUP = true;  
